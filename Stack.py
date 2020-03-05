c9661a2c4fd21e89e87d6beee82d57b9bc7db2ef
fullere <fullere@hartwick.edu>
2020-03-05 00:04:35 -0500

    write code

--- Stack.py
+++ Stack.py
@@ -1,3 +1,16 @@
 # Elizabeth Fuller
 # 3/4/20
 # Stack
+
+from linkedlist import SinglyLinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.new_list = SinglyLinkedList()
+
+    def push(self, data):
+        self.new_list.prepend()
+
+    def pop(self):
+        return self.new_list.remove_from_head()
